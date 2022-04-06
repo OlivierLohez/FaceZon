4884cf64e0d21f9a60af85b5f39c780a78c8eb59
Lohez <olivier.lohezz@gmail.com>
2022-04-06 13:58:30 +0200

    Création de l'entité Article

---
+++ src/Entity/Article.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 200)]
+    private $titre;
+
+    #[ORM\Column(type: 'text')]
+    private $contenuArticle;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getContenuArticle(): ?string
+    {
+        return $this->contenuArticle;
+    }
+
+    public function setContenuArticle(string $contenuArticle): self
+    {
+        $this->contenuArticle = $contenuArticle;
+
+        return $this;
+    }
+}
