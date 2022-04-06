7683b0cc34cccbff0c6913d9a7526ba1f61f5795
Lohez <olivier.lohezz@gmail.com>
2022-04-06 14:34:12 +0200

    formulaire Article

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ArticleType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use App\Entity\Article;
 
 class BaseController extends AbstractController
 {
@@ -13,6 +18,30 @@ class BaseController extends AbstractController
     {
         return $this->render('base/accueil.html.twig', [
             'controller_name' => 'BaseController',
+        ]);
+    }
+
+    #[Route('/article',name:'article')]
+        public function article(Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        if($request->isMethod('POST')){
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted()&&$form->isValid()){
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($article);
+                $em->flush();
+                $this->addFlash('notice','Article envoyé');
+                return $this->redirectToRoute('article');
+            }
+        }
+        return $this->render('base/article.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
